79cf1df5af16f66a9617a306ace1dcd8cb0d03d1
LastDemon99 <luistorres2346@gmail.com>
2024-03-05 15:30:44 -0300

    Update Semantic Token Provider
    The semantic provider now filters by blocks '{}'

--- src/features/semanticTokenProvider.ts
+++ src/features/semanticTokenProvider.ts
@@ -3,7 +3,7 @@ import * as vscode from 'vscode';
 const tokenTypes = new Map<string, number>();
 const tokenModifiers = new Map<string, number>();
 
-const tokenTypesLegend = ['variable'];
+const tokenTypesLegend = ['variable', 'function'];
 const tokenModifiersLegend = ['declaration', 'usage'];
 
 tokenTypesLegend.forEach((tokenType, index) => tokenTypes.set(tokenType, index));
@@ -14,32 +14,44 @@ export const legend = new vscode.SemanticTokensLegend(tokenTypesLegend, tokenMod
 export class semanticTokensProvider implements vscode.DocumentSemanticTokensProvider {
     provideDocumentSemanticTokens(document: vscode.TextDocument): vscode.ProviderResult<vscode.SemanticTokens> {
         const builder = new vscode.SemanticTokensBuilder(legend);
+
+        let inFunction = false;
         var variables = new Array<string>();
-        
-        for (let lineNumber = 0; lineNumber < document.lineCount; lineNumber++) {
-            const line = document.lineAt(lineNumber);
+
+        for (let i = 0; i < document.lineCount; i++) {
+            const line = document.lineAt(i);
             const text = line.text;
 
-            variables.forEach((var_declaration) => {
-                const regex = new RegExp(`(?<!")\\b(${var_declaration})\\b(?!")`, 'g');
-                let match;
-                if ((match = regex.exec(text)) !== null)
+            if (text.startsWith("{"))  inFunction = true;
+            if (inFunction) 
+            {
+                if (text.startsWith("}")) 
                 {
-                    const variableName = match[1];
-                    builder.push(lineNumber, match.index, variableName.length, this.getTokenTypeIndex('variable'), this.getTokenModifiers(['usage']));
+                    variables = new Array<string>();
+                    inFunction = false;
                 }
-            });
-
-            const var_declaration = /\b([a-zA-Z_]\w*)\b(?=\s*([-+*/%])?=)/g;
-            let match;
-            while ((match = var_declaration.exec(text)) !== null) {
-                const variableName = match[1];
-                variables.push(variableName);
-                builder.push(lineNumber, match.index, variableName.length, this.getTokenTypeIndex('variable'), this.getTokenModifiers(['declaration']));
+                else this.processFunctionBlock(builder, text, i, variables);
             }
         }
-
+        
         return builder.build();
+    }
+
+    private processFunctionBlock(builder: vscode.SemanticTokensBuilder, line: string, lineIndex: number, variables: Array<string>) {        
+        let match;
+        
+        variables.forEach((var_declaration) => {
+            const regex = new RegExp(`(?<!")\\b(${var_declaration})\\b(?!")`, 'g');
+            if ((match = regex.exec(line)) !== null)
+                builder.push(lineIndex, match.index, (match[1]).length, this.getTokenTypeIndex('variable'), this.getTokenModifiers(['usage']));
+        });
+
+        let regex = /\b([a-zA-Z_]\w*)\b(?=\s*([-+*/%])?=)/g;
+        while ((match = regex.exec(line)) !== null) {
+            const variableName = match[1];
+            variables.push(variableName);
+            builder.push(lineIndex, match.index, variableName.length, this.getTokenTypeIndex('variable'), this.getTokenModifiers(['declaration']));
+        }
     }
 
     private getTokenTypeIndex(tokenType: string): number {
